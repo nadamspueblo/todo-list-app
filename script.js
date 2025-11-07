94be781b69bbb0db627f74a3dbe9862b9edcd648
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2025-11-07 17:42:58 +0000

    Publish ready

--- script.js
+++ script.js
@@ -4,34 +4,58 @@ function addTask(event) {
     // Get user input from textbox
     let textbox = document.getElementById("task-input");
     let task = textbox.value;
+    // Clear text box
+    textbox.value = "";
+
+    // Check for empty task text
+    if (task == "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let idNum = generateIdNum();
     
-    createTaskDiv(task);
+    createTaskDiv(task, idNum);
 
     // Save task to local storage
-    localStorage.setItem(taskDiv.id, task);
+    localStorage.setItem("task" + idNum, task);
 
     console.log(localStorage.length);
 }
 
-function createTaskDiv(task) {
+function generateIdNum() {
+    //let todoList = document.getElementById("todo-list");
+    //let idNum = todoList.childElementCount;
+    //return idNum;
+
+    // Iterate through local storage
+    // Check for first available id number
+    let idNum = 0;
+    while (localStorage.getItem("task" + idNum) != null) {
+        idNum++;
+    }
+    return idNum;
+}
+
+function createTaskDiv(task, idNum) {
     // Create a list item
     // Get todo-list container div
     let todoList = document.getElementById("todo-list");
 
     // Create list-item div
     let taskDiv = document.createElement("div");
-    taskDiv.id = "task" + todoList.childElementCount;
+    taskDiv.id = "task" + idNum;
     taskDiv.classList.add("list-item");
 
     // Create checkbox
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.id = "checkbox" + todoList.childElementCount;
+    checkbox.id = "checkbox" + idNum;
     checkbox.addEventListener("change", removeTask);
 
     // Create label
     let label = document.createElement("label");
-    label.id = "label" + todoList.childElementCount;
+    label.id = "label" + idNum;
     label.innerText = task;
 
     // Append checkbox to list-item div
@@ -60,6 +84,7 @@ function removeTask(event) {
     // Remove the task div from layout
     setTimeout(function() {
         taskDiv.remove();
+        localStorage.removeItem(taskDiv.id);
     }, 1000);
     
 }
@@ -71,9 +96,10 @@ function loadTasks() {
         let key = localStorage.key(i);
         let task = localStorage.getItem(key);
         console.log(task);
+        // Create task divs for each task
+        createTaskDiv(task, key.substring(4));
     }
 
-    // Create task divs for each task
 }
 
 loadTasks();
